e53a7323887166c39b57f12488f6f09ee1265c01
H-Acht <sasuke204102@gmail.com>
2023-05-24 13:55:38 +0900

    ルールシーン追加

---
+++ FlappyBird/SceneRule.cpp
@@ -0,0 +1,1 @@
+#include "SceneRule.h"
---
+++ FlappyBird/SceneRule.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "SceneBase.h"
+
+class SceneRule : public SceneBase
+{
+public:
+	SceneRule();
+	~SceneRule();
+
+	void Init();
+	void Update();
+	void Draw();
+private:
+};
+
