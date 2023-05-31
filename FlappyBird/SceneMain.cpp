bcdbdffaf33bdf1664800c725b94a013d37e2cf7
H-Acht <sasuke204102@gmail.com>
2023-05-31 09:25:29 +0900

    ゲームのメイン画面

--- FlappyBird/SceneMain.cpp
+++ FlappyBird/SceneMain.cpp
@@ -4,14 +4,28 @@
 #include "Block.h"
 #include "BackGround.h"
 #include "Score.h"
+#include "SceneTitle.h"
 
 SceneMain::SceneMain() :
 	m_isEnd(false),
 	m_pPlayer(nullptr),
 	m_pBack(nullptr),
 	m_pBlock(nullptr),
-	m_pScore(nullptr)
+	m_pScore(nullptr),
+	titleFont(0),
+	Font(0),
+	select(0),
+	returnFlag(false),
+	selectFlag(false)
 {
+	//リソース読み込み
+	font_path = "Data/Font/azuki.ttf";
+	if (AddFontResourceEx(font_path, FR_PRIVATE, NULL) > 0) {
+	}
+	else {
+		// フォント読込エラー処理
+		MessageBox(NULL, "フォント読込失敗", "", MB_OK);
+	}
 }
 
 SceneMain::~SceneMain()
@@ -20,11 +34,21 @@ SceneMain::~SceneMain()
 	m_pBack = nullptr;
 	m_pBlock = nullptr;
 	m_pScore = nullptr;
-
+	
 	delete(m_pPlayer);
 	delete(m_pBack);
 	delete(m_pBlock);
 	delete(m_pScore);
+
+	DeleteFontToHandle(titleFont);
+	DeleteFontToHandle(Font);
+
+	if (RemoveFontResourceEx(font_path, FR_PRIVATE, NULL)) {
+	}
+	else {
+		MessageBox(NULL, "remove failure", "", MB_OK);
+	}
+
 }
 
 void SceneMain::init()
@@ -40,16 +64,46 @@ void SceneMain::init()
 	m_pBack->Init();
 	m_pBlock->Init();
 	m_pScore->Init();
+
+
+	titleFont = CreateFontToHandle("あずきフォント", 64, -1, -1);
+	Font = CreateFontToHandle("あずきフォント", 48, -1, -1);
+
+	m_pos.x = 500;
+	m_pos.y = -600;
+
+	m_textPos.x = 620;
+	m_textPos.y = -500;
+
+	m_textPos2.x = 600;
+	m_textPos2.y = -250;
+
+	m_textPos3.x = 780;
+	m_textPos3.y = -250;
+
+	m_hiScore.x = 690;
+	m_hiScore.y = -400;
 }
 
 SceneBase* SceneMain::update()
 {
 	// プレイヤーのアップデート処理
-	m_pPlayer->Update(*m_pBlock);
-	m_pBack->Update();
-	m_pBlock->Update();
-	m_pScore->Update(*m_pBlock);
-
+	if (m_pPlayer->gameOver == false)
+	{
+		m_pPlayer->Update(*m_pBlock);
+		m_pBack->Update();
+		m_pBlock->Update();
+		m_pScore->Update(*m_pBlock);
+	}
+	else
+	{
+		GameOverScreenUpdate();
+	}
+	
+	if (returnFlag == true)
+	{
+		return (new SceneTitle);
+	}
 
 	return this;
 }
@@ -61,6 +115,95 @@ void SceneMain::draw()
 	m_pPlayer->Draw();
 	m_pBlock->Draw();
 	m_pScore->Draw();
+	
+	GameOverScreenDraw();
+}
 
-	DrawFormatString(0, 60, GetColor(255, 255, 255), "Score -> %d", m_pBlock->passCount);
+void SceneMain::GameOverScreenUpdate()
+{
+	if (m_pos.y < 200)
+	{
+		m_pos.y += 10;
+		m_textPos.y += 10;
+		m_textPos2.y += 10;
+		m_textPos3.y += 10;
+		m_hiScore.y += 10;
+	}
+	else
+	{
+		selectFlag = true;
+	}
+
+	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+	static int push = 0;
+
+	if (selectFlag == true)
+	{
+		if (select == 0)
+		{
+			m_textPos2.x = 600;
+			m_textPos2.y = 550;
+
+			m_textPos2.x += GetRand(5);
+			m_textPos2.y += GetRand(5);
+
+			if (padState & PAD_INPUT_LEFT || padState & PAD_INPUT_RIGHT)
+			{
+				if (push == 0)
+				{
+					select = 1;
+				}
+				push = 1;
+			}
+			else
+			{
+				push = 0;
+			}
+
+			if (padState & PAD_INPUT_1)
+			{
+				selectFlag = false;
+				SceneMain::init();
+			}
+		}
+		if (select == 1)
+		{
+			m_textPos3.x = 780;
+			m_textPos3.y = 550;
+
+			m_textPos3.x += GetRand(5);
+			m_textPos3.y += GetRand(5);
+
+			if (padState & PAD_INPUT_LEFT || padState & PAD_INPUT_RIGHT)
+			{
+				if (push == 0)
+				{
+					select = 0;
+				}
+				push = 1;
+			}
+			else
+			{
+				push = 0;
+			}
+
+			if (padState & PAD_INPUT_1)
+			{
+				selectFlag = false;
+				returnFlag = true;
+			}
+		}
+	}
+}
+
+void SceneMain::GameOverScreenDraw()
+{
+	DrawBox(m_pos.x - 5, m_pos.y - 5, m_pos.x + 505, m_pos.y + 505, GetColor(0, 0, 0), true);
+	DrawBox(m_pos.x, m_pos.y, m_pos.x + 500, m_pos.y + 500, GetColor(255, 255, 255), true);
+	DrawStringToHandle(m_textPos.x, m_textPos.y, "Gameover", GetColor(0, 0, 0), titleFont);
+	DrawStringToHandle(m_textPos2.x, m_textPos2.y, "Retry", GetColor(0, 0, 0), Font);
+	DrawStringToHandle(m_textPos3.x, m_textPos3.y, "Title", GetColor(0, 0, 0), Font);
+
+	DrawStringToHandle(m_hiScore.x, m_hiScore.y, "Score", GetColor(0, 0, 0), Font);
+	
 }
--- FlappyBird/SceneMain.h
+++ FlappyBird/SceneMain.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "SceneBase.h"
-
+#include "Vec2.h"
 
 class Player;
 class BackGround;
 class Block;
 class Score;
+class GameOverScreen;
 
 class SceneMain : public SceneBase
 {
@@ -18,8 +19,12 @@ public:
 
 	virtual SceneBase* update() override;
 	virtual void draw() override;
-
+	
 	virtual bool isEnd() { return m_isEnd; }
+
+	//ゲームオーバー画面の表示
+	void GameOverScreenUpdate();
+	void GameOverScreenDraw();
 
 private:
 	bool m_isEnd;
@@ -29,4 +34,23 @@ private:
 	BackGround* m_pBack;
 	Block* m_pBlock;
 	Score* m_pScore;
+	
+private:
+
+	Vec2 m_pos;
+	Vec2 m_textPos;
+	Vec2 m_textPos2;
+	Vec2 m_textPos3;
+	Vec2 m_hiScore;
+
+	LPCSTR font_path;
+
+	int titleFont;
+	int Font;
+
+	int select;
+
+	bool returnFlag;
+	bool selectFlag;
+	
 };
